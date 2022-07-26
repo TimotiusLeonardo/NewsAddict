596e8bfe3e347088860daa8477b488c267a6b3dd
Timotius Leonardo <60493932+TimotiusLeonardo@users.noreply.github.com>
2022-07-26 23:00:09 +0700

    Initial Commit

---
+++ NewsAddict/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NewsAddict
+//
+//  Created by Timotius Leonardo Lianoto on 26/07/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
